1ffcc671f0184d8da6640ec5ef0b10390acb1f29
shadowmedi <shadowmeditation@outlook.com>
2022-04-10 20:19:16 +0800

    update new ways

--- do_exercise/my_strlen.c
+++ do_exercise/my_strlen.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <assert.h>
-int my_strlen(const char* arr)
+#include<assert.h>
+int my_strlen1(const char* arr)
 {
 	assert(arr);//断言不为空指针
 	int num = 0;//统计数目
@@ -12,9 +12,30 @@ int my_strlen(const char* arr)
 	arr = NULL;
 	return num;
 }
+
+size_t my_strlen(const char* arr)
+{
+	assert(arr);//断言不为空指针
+	if (*arr == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(arr + 1);
+}
+
+size_t my_strlen3(const char* arr)
+{
+	assert(arr);//断言不为空指针
+	const char* init = arr;//保留arr的初始值
+	while (*arr)//使arr指向'\0'
+	{
+		arr++;
+	}
+	size_t num = arr - init;//两个指针相减得到它们之间的元素个数
+	return num;
+}
 int main()
 {
-	char arr1[10] = "abcdehi";
-	printf("%d", my_strlen(arr1));
+	char arr1[10] = "abc";
+	printf("%d", (int)my_strlen3(arr1));
 	return 0;
 }
